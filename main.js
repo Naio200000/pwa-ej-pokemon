520b76574b1f3022c6e0774605876aa1cafc6283
Naio200000 <90395089+Naio200000@users.noreply.github.com>
2023-10-03 21:51:23 -0300

    feat: funciona la prueba con la api

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 const poke = fetch (`https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`)
                 .then(data => data.json())
-                .then(({data}) => data)
                 .then(data => console.log(data))
