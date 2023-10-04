0da06571cf79876aa403d7c751effaa2d5e8e5e2
Nicolas Alsinet <nicolas.alsinet@gmail.com>
2023-10-04 09:57:41 -0300

    feat: add card template

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-const poke = fetch (`https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`)
-                .then(data => data.json())
-                .then(data => console.log(data))
+const getPokes = () => {
+    return fetch (`https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`)
+        .then(rta => rta.json())
+        .then((data) => data.results)
+        .then(results => results.map((pokemon) =>{
+            let {name, url} = pokemon;
+            return {
+                name,
+                url
+            }
+        }));
+}
